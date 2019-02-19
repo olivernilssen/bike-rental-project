35b29befca7a5c42ea6509480e8269bd6da6ef73
Silje Tanemsmo <siljetanemsmo@gmail.com>
2019-02-19 16:49:32 +0100

    Lagt inn noen spørringer

--- src/services.js
+++ src/services.js
@@ -1,2 +1,52 @@
 import { connection } from './mysql_connection';
 
+class RentalService {
+  getAvailableBikes(success) {
+    connection.query(
+      'select * from Bikes where object_id in (select object_id from RentalObjects where objectStatus = "OK"))',
+      (error, results) => {
+        if (error) return console.error(error);
+
+        success(results);
+      }
+    );
+  }
+
+  getBike(id, success) {
+    connection.query('select * from Bikes where object_id = ?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0]);
+    });
+  }
+
+  getBikesByLocation(location, success) {
+    connection.query(
+      'select * from Bikes where object_id in (select object_id from RentalObjects where currentLocation = ?))',
+      [location],
+      (error, results) => {
+        if (error) return console.error(error);
+
+        success(results);
+      }
+    );
+  }
+
+  getCustomers(success) {
+    connection.query('select * from Customers', (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+
+  getCustomer(id, success) {
+    connection.query('select * from Customers where customer_id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0]);
+    });
+  }
+}
+
+export let rentalService = new RentalService();
